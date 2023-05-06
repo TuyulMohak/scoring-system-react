c7381a2bba2850ef883333121bf5fa1418d7e23d
Muhammad Rivandra <rivandra0@gmail.com>
2023-05-06 08:08:16 +0700

    changing only desktop screen to purple

--- src/components/OnlyInDesktop.jsx
+++ src/components/OnlyInDesktop.jsx
@@ -1,7 +1,7 @@
 
 export default function () {
 	return (
-		<div className="h-screen w-screen fixed top-0 left-0 bg-green-900 z-10 grid justify-center items-center">
+		<div className="h-screen w-screen fixed top-0 left-0 bg-purple-900 z-10 grid justify-center items-center">
 			<h3 className="text-3xl text-white block p-3">
 				<p className="text-sm text-gray-100 block">Scoring System with React, By Muhammad Rivandra</p>
 				Optimal Experience only on Desktop 💻
